a5abc09b578623037db64776682e43bdbaffe877
gchan-eu <82727323+gchan-eu@users.noreply.github.com>
2026-01-01 19:05:11 +0200

    Refactor sheet handlers: move functions to a dedicated file for better organization and maintainability

--- Code.js
+++ Code.js
@@ -32,49 +32,6 @@ function onEdit(e) {
 }
 
 
-// ---------- SHEET HANDLERS ----------
-
-function handleOwnershipSet(e) {
-  assignOwnershipSetId(e);
-}
-
-function handleOwnerShare(e) {
-  assignOwnerShareId(e);
-  // add more OWNER_SHARE features here
-}
-
-function handleOvernightStay(e) {
-  assignOvernightStayId(e);
-  // add more OVERNIGHT_STAYS features here
-}
-
-function handleSplitMethods(e) {
-  assignSplitMethodId(e);
-  // add more SPLIT_METHODS features here
-}
-
-function handleExpenses(e) {
-  const sheet = e.source.getActiveSheet();
-  const col = e.range.getColumn();
-  const actionCol = getColumnIndexByHeader(sheet, "Action");
-
-  // If user edited the Action column, DO NOT run assignExpenseId
-  if (col === actionCol) {
-    handleExpenseActions(e);
-    return;
-  }
-
-  // Otherwise, normal behavior
-  assignExpenseId(e);
-}
-
-
-function handleTransactions(e) {
-  assignTransactionId(e);
-  // add more TRANSACTIONS features here
-}
-
-
 // ---------- EXPENSE ACTIONS ----------
 
 function handleExpenseActions(e) {
